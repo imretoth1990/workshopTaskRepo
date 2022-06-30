0838d256bab5f490a97a259880d8a0e59c054528
Gergo Kovacs <gergosmith306@gmail.com>
2022-06-30 17:32:49 +0200

    Update 1) .replace() method -> .replaceAll(), 2) second parameter of the method: red -> yellow

--- task.js
+++ task.js
@@ -1,9 +1,9 @@
 // program to replace a character of a string
 
-const string = 'Mr Red has a red house and a red car';
+const string = "Mr Red has a red house and a red car";
 
 // replace the characters
-const newText = string.replace('red', 'red');
+const newText = string.replaceAll("red", "yellow");
 
 // display the result
 console.log(newText);
